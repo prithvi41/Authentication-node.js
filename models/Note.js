8a18fa48af07909e66a8d90a79cebb11b6b2c9ed
Prithvi41 <rajp7163@gmail.com>
2024-03-30 12:45:39 +0530

    CRUD APIs for notes

--- models/Note.js
+++ models/Note.js
@@ -11,18 +11,68 @@ class Note {
             throw new Error("Content required");
         }
     }
+    // craete new note
     async save() {
         try {
             await this.validate();
-            await client.query("INSERT INTO notes (title, content, userId) VALUES ($1, $2, $3)", [
-                this.title, this.content, this.userId
+            const currentTimestamp = new Date();
+            await client.query("INSERT INTO notes (title, content, userId, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)", [
+                this.title, this.content, this.userId, currentTimestamp, currentTimestamp
             ]);
             return true;
         }
         catch(e) {
             console.error(e.message);
             throw new Error("Failed to save note");
-            return false;
+        }
+    }
+    // update existing note by id
+    async updateById(id, newData) {
+        try {
+            const existingNote = await client.query("SELECT * FROM notes WHERE id = $1 AND user_id = $2", [id, this.userId]);
+            if (existingNote .rows.length === 0) {
+                throw new Error("Note not found or user unauthorized to update");
+            }
+            const updateFields = [];
+            const values = [id, this.userId];
+            let paramIndex = 3; 
+            if (newData.title) {
+                updateFields.push(`title = $${paramIndex}`);
+                values.push(newData.title);
+                paramIndex++;
+            }
+            if (newData.content) {
+                updateFields.push(`content = $${paramIndex}`);
+                values.push(newData.content);
+                paramIndex++;
+            }
+            const currentTimestamp = new Date();
+            updateFields.push(`updated_at = $${paramIndex}`);
+            values.push(currentTimestamp);
+    
+            const updateQuery = `UPDATE notes SET ${updateFields.join(', ')} WHERE id = $1 AND user_id = $2`;
+            await client.query(updateQuery, values);
+            
+            return true;
+        } 
+        catch(e) {
+            console.error(e.message);
+            throw new Error("Failed to update note");
+        }
+    }
+    // get a note by id 
+    async getNoteById(id) {
+        try {
+            const existingNote = await client.query("SELECT * FROM notes WHERE id = $1 AND user_id = $2", [id, this.userId]);
+            if (existingNote.rows.length === 0) {
+                throw new Error("Note not found");
+            }
+            const { title, content } = existingNote.rows[0];
+            return { id, title, content };
+        }
+        catch(e) {
+        console.error(e);
+        throw new Error("Failed to fetch note");
         }
     }
 }
--- route/notes.js
+++ route/notes.js
@@ -15,7 +15,36 @@ router.post('/new', authenticateToken, async (req, res) => {
     }
     catch(e) {
         console.error(e);
-        res.json({ error: e.message });
+        res.status(500).json({ error: "Failed to create note" });
+    }
+});
+
+router.put('/:id', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { title, content } = req.body;
+        const noteId = req.params.id;
+        const notes = new Note({ title, content, userId });
+        await notes.validate();
+        await notes.updateById(noteId, { title, content });
+        res.status(200).json({ message: "Note updated successfully" });
+    } 
+    catch(e) {
+        console.error(e);
+        res.status(500).json({ error: "Failed to update note" });
+    }
+});
+
+router.get('/:id', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const noteId = req.params.id;
+        const note = new Note({ userId });
+        const fetchedNote = await note.getNoteById(noteId);
+        res.status(200).json(fetchedNote);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: "Failed to fetch note" });
     }
 });
 
